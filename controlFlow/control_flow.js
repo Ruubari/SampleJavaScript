4c75269564972da5e15f6ef6ededa5ddf29da4b9
Ruubari <roope.suojarinne@gmail.com>
2025-04-24 02:11:40 -0400

    Control FLow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,45 @@
+let userRole = "admin";
+let acccessLevel;
+let isLoggingIn = true;
+let userMessage;
+let userType = "subscriber";
+let userCategory;
+
+
+if (userRole === "admin") {
+    acccessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    acccessLevel = "Limitted access granted";
+} else {
+    acccessLevel = "No access granted";
+}
+
+if (isLoggingIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin";
+    } else {
+        userMessage = "Welcome, user!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+switch (userType) {
+    case "admin":
+        userCategory = "Adminstrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+
+
+console.log("Access Level:", acccessLevel);
+console.log("User Message:", userMessage);
+console.log("User Category", userCategory);
