3df8424208d935096abdc6b312fc1966a653521c
Ruubari <roope.suojarinne@gmail.com>
2025-05-14 10:06:06 -0400

    Calc total length of words

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -33,12 +33,17 @@ function endTest() {
     var wpm = 0; // Default value
 
     if (timeElapsed !== 0 && !isNaN(typedWords)) {
-        wpn = Math.round((typedWords / timeElapsed) * 60);
+        wpm = Math.round((typedWords / timeElapsed) * 60);
     }
+
+    // Calculating total length
+    let totalLength = userTypedText.length;
+
 
     // Display The results
     let outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + totalLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
