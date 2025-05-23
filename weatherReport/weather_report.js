b295b326a0c0a9c8eba064e369a2ce8416653d43
Ruubari <roope.suojarinne@gmial.com>
2025-05-23 02:41:28 -0400

    getting weather with coordinates

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,22 +1,27 @@
 function showweatherDetails(event) {
     event.preventDefault();
 
-    const city = document.getElementById('city').value;
+    const lat = document.getElementById('latitude').value;
+    const lon = document.getElementById('longitude').value;
     const apiKey = "38002a9e0ac05d99a9faabd927744d0d";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+
+
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const weatherInfo = document.getElementById('weatherInfo');
-            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+            const coordinatesInfo = document.getElementById('coordinatesInfo');
+            coordinatesInfo.innerHTML = `<h2>Weather at ${lat, lon}</h2>
+                                     <p>Weather: ${data.name}</p>
                                      <p>Temerature: ${data.main.temp} &#8451;</p>
                                      <p>Weather: ${data.weather[0].description}</p>`;
         })
         .catch(error => {
             console.error('Error fetching weather:', error);
-            const weatherInfo = document.getElementById('weatherInfo');
-            weatherInfo.innerHTML = `<p> Failed to fetch weather. Please try again.</p>`;
+            const coordinatesInfo = document.getElementById('coordinatesInfo');
+            coordinatesInfo.innerHTML = `<p> Failed to fetch weather. Please try again.</p>`;
         });
 
 }
